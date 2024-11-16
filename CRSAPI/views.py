55096e1f0dd78246f4dfe644cd8ddf4313ee0448
MoGHenry <moghenry0@gmail.com>
2024-11-15 23:31:27 -0500

    implement translator for CRSAPI

--- CRSAPI/views.py
+++ CRSAPI/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from CRSAPI.crop_prediction import predict_crop, create_dataframe
+from GoogleTrans.google_translate import translate_text, get_dest
 # Constants for error messages
 ERROR_MESSAGES = {
     'not_post': 'not using POST request',
@@ -22,10 +23,15 @@ def crop_recommendation_predict(request: HttpResponse) -> JsonResponse:
         # convert request.body to dataframe format for prediction
         data = request.body
         df = create_dataframe(data)
+        dest_language = get_dest(data)
         # predict crop using predict_crop function
         prediction_result = predict_crop(df)
+        if dest_language is None or dest_language == 'en':
+            translated_result = prediction_result
+        else:
+            translated_result = translate_text(prediction_result, dest_language)
         # return prediction result as json response
-        return JsonResponse({'error': 'false', 'prediction': prediction_result}, status=200)
+        return JsonResponse({'error': 'false', 'prediction': translated_result}, status=200)
     except ValueError as ve:
         return JsonResponse({'error': 'true', 'message': f"{ERROR_MESSAGES['invalid_input']} {ve}"}, status=400)
     except Exception as e:
