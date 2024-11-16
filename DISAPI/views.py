b6e0425356c355c31d9064c7a512edbfc5eb5d87
MoGHenry <moghenry0@gmail.com>
2024-11-15 23:47:11 -0500

    implement google translate for DIS API

--- DISAPI/views.py
+++ DISAPI/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from DISAPI.disease_prediction import predict_disease
+from GoogleTrans.google_translate import translate_text, get_dest
 # Constants for error messages
 ERROR_MESSAGES = {
     'not_post': 'not using POST request',
@@ -22,13 +23,18 @@ def disease_identification_predict(request: HttpResponse) -> JsonResponse:
             return JsonResponse({'error': 'true', 'message': ERROR_MESSAGES['invalid_content_type']}, status=400)
         # convert request.body to dataframe format for prediction
         photo = request.FILES.get('file')
+        dest_language = request.POST.get('dest')
         # check if picture is not empty
         if not photo:
             return JsonResponse({'error': 'true', 'message': ERROR_MESSAGES['no_photo']}, status=400)
         # predict disease using predict_disease function
         prediction_result = predict_disease(photo)
+        if dest_language is None or dest_language == 'en':
+            translated_result = prediction_result
+        else:
+            translated_result = translate_text(prediction_result, dest_language)
         # return prediction result as json response
-        return JsonResponse({'error': 'false', 'prediction': prediction_result}, status=200)
+        return JsonResponse({'error': 'false', 'prediction': translated_result}, status=200)
     except ValueError as ve:
         return JsonResponse({'error': 'true', 'message': f"{ERROR_MESSAGES['invalid_input']} {ve}"}, status=400)
     except Exception as e:
