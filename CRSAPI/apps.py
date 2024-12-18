5e59820da91fc384c833400fd8462d9774a02ac7
MoG Henry <84988222+MoGHenry@users.noreply.github.com>
2024-12-18 17:43:51 -0500

    Update apps.py

--- CRSAPI/apps.py
+++ CRSAPI/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CroprecommendationapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'CropRecommendationAPI'
+    name = 'CRSAPI'
